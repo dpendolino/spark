ced2c7a35fe7f049ba02c687a83cd3bc47169d0a
Daniel Pendolino <daniel@counterhack.com>
2019-09-20 14:16:58 -0400

    update reflector to suite my needs

--- roles/mirrorlist/files/reflector-update.sh
+++ roles/mirrorlist/files/reflector-update.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-/usr/bin/reflector --latest 20 \
-                   --sort rate \
-                   --protocol https \
-                   --save /etc/pacman.d/mirrorlist
-                   
+reflector --verbose \
+          --country 'United States' \
+          -l 200 \
+          -p https \
+          --sort rate \
+          --save /etc/pacman.d/mirrorlist
+
+
 if [[ -f /etc/pacman.d/mirrorlist.pacnew ]]; then
     rm /etc/pacman.d/mirrorlist.pacnew
 fi
